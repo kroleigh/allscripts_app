0def137903040b9e6752c31b96ea8d50a5417e04
Your Name <your@email.com>
2011-09-15 14:15:08 -0500

    Pushing new changes

--- source/nothinbutdotnetstore.specs/ViewTheMainDepartmentsInTheStoreSpecs.cs
+++ source/nothinbutdotnetstore.specs/ViewTheMainDepartmentsInTheStoreSpecs.cs
@@ -1,4 +1,6 @@
-﻿using Machine.Specifications;
+﻿using System.Data;
+using Machine.Specifications;
+using developwithpassion.specifications.extensions;
 using developwithpassion.specifications.rhinomocks;
 using nothinbutdotnetstore.web.application;
 using nothinbutdotnetstore.web.core;
@@ -15,22 +17,28 @@ namespace nothinbutdotnetstore.specs
 
         public class when_processing_a_request : concern
         {
-            Establish c = () =>
-            {
-                request = fake.an<IContainRequestInformation>();
-            };
+          Establish c = () =>
+          {
+            request = fake.an<IContainRequestInformation>();
+            depends.on<RequestCriteria>(x => true);
+            application_behaviour = depends.on<IPerformApplicationBehaviour>();
+            cmd = depends.on<IDbCommand>();
 
-            Because b = () =>
-                sut.process(request);
+          };
+
+          Because b = () =>
+              sut.process(request);
 
 
-            It should_get_a_list_of_the_main_departments_in_the_store = () =>
-            {
+          It should_get_a_list_of_the_main_departments_in_the_store = () =>
+          {
+            cmd.received(x => x.ExecuteReader());
+          };
+          
+          static IContainRequestInformation request;
+          static IPerformApplicationBehaviour application_behaviour;
+          static IDbCommand cmd;
 
-            };
-                
-
-            static IContainRequestInformation request;
         }
     }
 }
--- source/nothinbutdotnetstore/web/application/ViewTheMainDepartmentsInTheStore.cs
+++ source/nothinbutdotnetstore/web/application/ViewTheMainDepartmentsInTheStore.cs
@@ -1,12 +1,22 @@
-﻿using nothinbutdotnetstore.web.core;
+﻿using System.Data;
+using nothinbutdotnetstore.web.core;
 
 namespace nothinbutdotnetstore.web.application
 {
     public class ViewTheMainDepartmentsInTheStore : IPerformApplicationBehaviour
     {
-        public void process(IContainRequestInformation request)
-        {
-            throw new System.NotImplementedException();
-        }
+      IDbCommand command;
+      public ViewTheMainDepartmentsInTheStore(IDbCommand command)
+      {
+        this.command = command;
+      }
+
+      public void process(IContainRequestInformation request)
+      {
+        //using(IDataReader reader = command.ExecuteReader())
+        //{
+
+        //}
+      }
     }
 }
